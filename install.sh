2813d5e486bb42c0f19e46ff37c7cff4bac77bc4
camielverdult <camiel@camiel.pw>
2021-06-02 17:22:00 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # This scripts install all the prerequisites for the RaceX car to run 
+apt install cmake openssl -y
+export OPENSSL_ROOT_DIR=$(whereis openssl)
 python3 -m pip install -r pip_requirements.txt

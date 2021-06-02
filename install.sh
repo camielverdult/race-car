cb5e510932472001c705a0b45ddb7566c8360db0
camielverdult <camiel@camiel.pw>
2021-06-02 18:03:46 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,46 @@
 #!/usr/bin/env bash
 
 # This scripts install all the prerequisites for the RaceX car to run 
-apt install cmake openssl -y
-export OPENSSL_ROOT_DIR=$(whereis openssl)
-python3 -m pip install -r pip_requirements.txt
+apt install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran python3-dev -y
+
+cd ~
+wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.5.2.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/refs/tags/4.5.2.zip
+
+unzip opencv.zip -d opencv
+unzip opencv_contrib.zip -d opencv_contrib
+
+echo "CONF_SWAPSIZE=2048" > /etc/dphys-swapfile
+
+/etc/init.d/dphys-swapfile stop
+/etc/init.d/dphys-swapfile start
+
+python3 -m pip install numpy
+
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D CMAKE_SHARED_LINKER_FLAGS=-latomic \
+    -D BUILD_EXAMPLES=OFF ..
+
+make -j4
+make install
+ldconfig
+
+echo "CONF_SWAPSIZE=100" > /etc/dphys-swapfile
+
+/etc/init.d/dphys-swapfile stop
+/etc/init.d/dphys-swapfile start
+
+mkdir /usr/local/lib/python3.7/site-packages/cv2/python-3.7
+cp cv2.cpython-*.so c/usr/local/lib/python3.7/site-packages/cv2/python-3.7/cv2.so
+
+python3 -m pip -r pip_requirements.txt

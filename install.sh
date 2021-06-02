28ba9829a575c87c5f8d4fdf58a0a529770ad605
camielverdult <camiel@camiel.pw>
2021-06-02 18:12:38 +0200

    update path

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ echo "CONF_SWAPSIZE=2048" > /etc/dphys-swapfile
 
 python3 -m pip install numpy
 
-cd ~/opencv
+cd /home/pi/opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D OPENCV_EXTRA_MODULES_PATH=/home/pi/opencv_contrib/modules \
     -D ENABLE_NEON=ON \
     -D ENABLE_VFPV3=ON \
     -D BUILD_TESTS=OFF \

292ed57a13d3a62510a1116cf7ed5647fe7103b7
camielverdult <camiel@camiel.pw>
2021-06-04 16:29:08 +0200

    update hw test

--- hw_test.py
+++ hw_test.py
@@ -4,6 +4,8 @@ import tweaking, gpiozero, time
 motor = MotorShield()
 servo = gpiozero.AngularServo(tweaking.servo_pin)
 
+servo.angle = tweaking.servo_middle
+
 for i in range(0, 100):
     print(float(i) / 100)
     motor.drive_forwards(float(i) / 100)

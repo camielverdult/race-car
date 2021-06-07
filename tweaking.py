b4e3d123c9259f08f6f86f55fefb0c36df3ac3f1
camielverdult <camiel@camiel.pw>
2021-06-07 15:09:45 +0200

    steer after bigger angle

--- tweaking.py
+++ tweaking.py
@@ -11,7 +11,7 @@ servo_left = 23
 servo_right= -50
 servo_middle = (servo_left + servo_right) / 2 #-16
 nerf_angle = 10
-steer_after_angle = 5
+steer_after_angle = 10
 
 servo_mapping_values = [50, -50]
 

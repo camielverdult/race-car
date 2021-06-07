d2de27673783cd9fc01f0efac9fa95a9a1cc9eec
camielverdult <camiel@camiel.pw>
2021-06-07 15:13:42 +0200

    d

--- libraries/hardware_interfacer.py
+++ libraries/hardware_interfacer.py
@@ -48,7 +48,7 @@ class HwInterfacer:
         self.power_sensor = None
 
         self.motor.drive_forwards(1.00)
-        time.sleep(0.01)
+        time.sleep(0.1)
         self.motor.drive_forwards(tweaking.motor_speed_range[0])
 
     # This function is called when an object is close to us

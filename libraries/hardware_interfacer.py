06ee62819c6f39173e641f7cab97a3a4657ec68f
camielverdult <camiel@camiel.pw>
2021-06-07 17:48:17 +0200

    df

--- libraries/hardware_interfacer.py
+++ libraries/hardware_interfacer.py
@@ -130,6 +130,14 @@ class HwInterfacer:
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
 
+                cv2.line(crop_img,(cx,0),(cx,720),(255,0,0),1)
+
+                cv2.line(crop_img,(0,cy),(1280,cy),(255,0,0),1)
+
+        
+
+                cv2.drawContours(crop_img, contours, -1, (0,255,0), 1)
+
                 print(cx)
 
                 if cx >= 120:

03f56919e5c088fc5adc0ad791ea49eefdd6f3e2
camielverdult <camiel@camiel.pw>
2021-06-07 16:00:37 +0200

    s

--- tweaking.py
+++ tweaking.py
@@ -11,8 +11,8 @@ servo_left = 23
 servo_right= -50
 servo_middle = (servo_left + servo_right) / 2 #-16
 nerf_angle = 10
-steer_after_angle = 20
-steer_after_angle_two = 30
+steer_after_angle = 7
+steer_after_angle_two = 25
 
 servo_mapping_values = [50, -50]
 

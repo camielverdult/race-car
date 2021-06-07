fc5320ead6268c84cc265c4bc6a36c350264535f
camielverdult <camiel@camiel.pw>
2021-06-07 15:10:28 +0200

    s

--- tweaking.py
+++ tweaking.py
@@ -11,9 +11,9 @@ servo_left = 23
 servo_right= -50
 servo_middle = (servo_left + servo_right) / 2 #-16
 nerf_angle = 10
-steer_after_angle = 10
+steer_after_angle = 5
 
-servo_mapping_values = [50, -50]
+servo_mapping_values = [40, -40]
 
 motor_speed_range = [0.03, 0.05]
 motor_max = 0.1

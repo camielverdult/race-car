d371a2035dfccb9b9a5c3f038fd55e5df5dfd2bf
camielverdult <camiel@camiel.pw>
2021-06-08 13:25:01 +0200

    s

--- libraries/hardware_interfacer.py
+++ libraries/hardware_interfacer.py
@@ -106,7 +106,7 @@ class HwInterfacer:
         video_capture.set(3, 160)
         video_capture.set(4, 120)
 
-        self.motor.drive_forwards(tweaking.motor_speed_range[0] + 0.1)
+        self.motor.drive_forwards(tweaking.motor_speed_range[0])
 
         while asyncio.get_event_loop().is_running():
 

43007edb6e6d9b92f135626cab6f2e274ee08554
camielverdult <camiel@camiel.pw>
2021-06-08 13:30:46 +0200

    s

--- libraries/hardware_interfacer.py
+++ libraries/hardware_interfacer.py
@@ -183,7 +183,7 @@ class HwInterfacer:
 
             else:
                 self.servo.angle = tweaking.servo_right
-                self.motor.drive_forwards(tweaking.motor_speed_range[0] + 0.007)
+                self.motor.drive_forwards(tweaking.motor_speed_range[0])
 
             # helling = 0
 
